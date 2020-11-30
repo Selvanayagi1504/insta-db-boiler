46feb5dcdd3c0b2a70650b89f6b463038b8fcd0e
yogesh056 <yogeshwaran.ramesh@codingmart.com>
2020-11-30 16:01:22 +0530

    u

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('dajbuu54nbotmo', 
-                                'qyyndnsojozmbz', 
-                                'edfb96e964c7d774479f424bad93bfbbdf71045619d06909a4ca4b92315d2d5a', {
+const sequelize = new Sequelize('d4d3rbe1mlcp76', 
+                                'taheaqrgyfjipe', 
+                                'f3eb0d51bcc3e189b61b0cca00088182fc9dd027285a69743ad9ed36585bff39', {
   host: 'ec2-3-210-23-22.compute-1.amazonaws.com',
   port:5432,
   dialect: 'postgres',
